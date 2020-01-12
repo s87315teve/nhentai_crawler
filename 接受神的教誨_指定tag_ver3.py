74a4f128a814eb9cf81a6f5e03a1d2691d028a7e
s87315teve <37323304+s87315teve@users.noreply.github.com>
2020-01-13 02:19:41 +0800

    tag_ver3_added

---
+++ 接受神的教誨_指定tag_ver3.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+from bs4 import BeautifulSoup
+import threading
+import queue
+import time
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, data_queue,path):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.data_queue = data_queue
+        self.path=path
+    def run(self):
+        download(self.data_queue,self.path)
+
+
+def download(data_queue,path):
+    global now_index
+    global limit_index
+    global elem_list
+    while not exitFlag:
+        #queueLock.acquire()
+        if not path_queue.empty():
+            filepath=data_queue.get()
+            pic = requests.get(filepath) #變數名稱為pic
+            index=filepath.rfind('/')
+            filename=filepath[index+1:]  #圖片名稱
+            img = pic.content #變數名稱命名為img
+            pic_out = open(path+filename,'wb') #
+            pic_out.write(img) #將圖片存入img
+            pic_out.close() #關閉檔案
+            #print(filename+" done")
+
+
+tag=str(input("請輸入你想要的tag(用空格分開):"))
+no_tag=str(input("請輸入你不要的tag(用空格分開):"))
+download_num=int(input("請輸入想要載幾本(將由 新->舊 的順序下載):"))
+
+tag_list=[]
+no_tag_list=[]
+language_list=[]
+artist_list=[]
+#輸入tag 
+tag=tag.upper()
+tag_list=tag.split()
+print(tag_list)
+#輸入不要的tag
+no_tag=no_tag.upper()
+no_tag_list=no_tag.split()
+print(no_tag_list)
+#輸入語言
+#language=language.upper()
+#language_list=language.split()
+#輸入作者
+#artist=artist.upper()
+#artist_list=artist.split()
+
+
+
+print("尋找中...")
+start_time_all=time.time()
+
+
+now_index=-1
+limit_index=0
+path_queue=queue.Queue()
+elem_list=None
+exitFlag=0
+
+
+def create(god_num,tag_list,no_tag_list):
+    #print("----------------------------------\n")
+    god_language=str(god_num)
+    length=len(god_language)
+    url="https://nhentai.net/g/"+god_language+"/"
+    r = requests.get(url) #將此頁面的HTML GET下來
+    soup = BeautifulSoup(r.text,'html.parser')
+    elem=soup.find("meta",attrs={"name":"twitter:description"})
+    try:
+        ifdownload=True
+        #print(elem)
+        tags=elem['content'].upper()
+        '''
+        if len(tag_list)>0:
+            for tag in tag_list:
+                if tags.find(tag)==-1:
+                    ifdownload=False
+                    return
+        '''
+        if len(no_tag_list)>0:
+            for no_tag in no_tag_list:
+                if tags.find(no_tag)!=-1:
+                    ifdownload=False
+                    return
+        if ifdownload==False:
+            return
+    except:
+        return
+    temp=url.rfind('/')
+    dirname=url[temp-length:temp] #將神的語言變成資料夾名稱
+    global now_index
+    global limit_index
+    global path_queue
+    global elem_list
+    global exitFlag
+    now_index=-1
+    limit_index=0
+    path_queue=queue.Queue()
+    elem_list=None
+    exitFlag=0
+
+    try:
+        start_time=time.time()
+        r = requests.get(url) #將此頁面的HTML GET下來
+        if r.status_code != 404:
+            #print(r.text) 印出HTML
+            soup = BeautifulSoup(r.text,'html.parser') 
+
+            #print(elem)
+            
+            elem_list=soup.find_all("div",class_="thumb-container")
+            #print(type(elem_list))
+            #for elem in elem_list:
+                #print (elem['data-src'])
+            #準備開始下載圖片
+            import os
+            if len(tag_list)>0 or len(no_tag_list)>0 or len(language_list)>0 or len(artist_list)>0:
+                path=""
+                if len(tag_list)>0:
+                    path = path+"yes"
+                    for tag in tag_list:
+                        path = path+ "_"+tag                   
+                if len(no_tag_list)>0:
+                    path = path+"_"
+                    path = path+"no"
+                    for no_tag in no_tag_list:
+                        path = path+"_"+no_tag
+                    path = path+"_"
+                if len(language_list)>0:
+                    path = path+"language"
+                    for language in language_list:
+                        path = path+"_"+language
+                    path = path+"_"
+                if len(artist_list)>0:
+                    path = path+"artist"
+                    for artist in artist_list:
+                        path =path+ "_"+artist
+            else:
+                path="不分類"
+            if not os.path.isdir(path):     
+                os.mkdir(path)        #建立以tag為名的資料夾
+            path=path+"\\"+dirname
+            if not os.path.isdir(path):     
+                os.mkdir(path)        #建立以神的語言為名的資料夾
+            path=path+"\\"
+
+
+
+            
+            print("現在正在下載 {}".format(god_num))
+            if soup.h1!=None:
+                temp=str(soup.h1)
+                temp=temp.replace("<h1>","")
+                temp=temp.replace("</h1>","")
+            print(temp)
+            if soup.h2!=None:
+                temp=str(soup.h2)
+                temp=temp.replace("<h2>","")
+                temp=temp.replace("</h2>","")
+                print(temp)
+            print("一共有 %d 個章節"%len(elem_list))
+            elem_list=soup.find_all("div",class_="thumb-container")
+            for elem in elem_list:
+                filepath=elem.img['data-src']
+                filepath=filepath.replace("https://t","https://i")
+                filepath=filepath.replace("t.",".")
+                path_queue.put(filepath)
+
+
+
+            #threadList = ["Thread-1", "Thread-2", "Thread-3","Thread-4","Thread-5","Thread-6","Thread-7","Thread-8","Thread-9","Thread-10","Thread-11","Thread-12"]
+            #queueLock = threading.Lock()
+            print("正在接受天之聲...")
+            threads = []
+            threadID = 1
+            n=1000
+            # 创建新线程
+            for tName in range(n):
+                thread = myThread(threadID,path_queue,path)
+                thread.start()
+                threads.append(thread)
+                threadID += 1
+
+            # 等待队列清空
+            while not path_queue.empty():
+                pass
+
+            # 通知线程是时候退出
+            exitFlag = 1
+
+            # 等待所有线程完成
+
+            for t in threads:
+                t.join()
+            print("神的語言"+dirname+"已接收完成")
+            end_time=time.time()
+            used_time=end_time-start_time
+            print("總共花費 {} 秒\n".format(used_time))
+
+
+        else:
+            print("這不是神的語言")
+
+
+    except KeyboardInterrupt:
+        return
+    except:
+        print("這不是神的語言，或是已經出bugㄌQQ")
+        return
+
+
+
+
+flag=True
+page=0
+count=0
+tag_list=tag.split()
+tag=""
+for t in tag_list:
+    tag=tag+"+"+t
+tag=tag.lstrip("+")
+print("search: "+tag)
+while flag==True:
+    page+=1
+    print("現在是第{}頁".format(page))
+    url="https://nhentai.net/search/?q="+tag+"&page="+str(page)      
+    r = requests.get(url) #將此頁面的HTML GET下來
+    soup = BeautifulSoup(r.text,'html.parser')
+    elem=soup.find("div","container index-container")
+    temp=elem.find_all("a")
+    
+    for t in temp:
+        if count<download_num:
+            count+=1
+            #將s變成神之語言
+            s=t["href"]
+            s=s.lstrip("/g")
+            s=s.rstrip("/")
+            create(s,tag_list,no_tag_list) #下載
+            
+        
+    if count>=download_num:
+        flag=False
+        
+        
+end_time_all=time.time()
+used_time=end_time_all-start_time_all
+print("\n\n合計花費 {} 秒".format(used_time))
+i=input("\n按任意鍵繼續...")
+
+
+
+
